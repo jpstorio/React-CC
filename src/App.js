4dd1cb4c4cf67132553c497beded1fee158a11af
Torio <jpstorio@mymail.mapua.edu.ph>
2022-06-05 19:29:27 +0800

    modify app + add data.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import './App.css';
 import './appStyles.css'
-import { Form } from './components/Form';
+import NAMES from './components/data.json'
+import { useState } from 'react';
+import { useTransition } from 'react';
 
-import styles from './appStyles.module.css'
-import { Greet } from './components/Greet';
-import { Message } from './components/Message';
-import { ClickHandler } from './components/ClickHandler';
-import { ParentComponent } from './components/ParentComponent';
-import { UserGreeting } from './components/UserGreeting';
-import { NameList } from './components/NameList';
-import { Stylesheet } from './components/Stylesheet';
-import { Inline } from './components/Inline';
-import { PostList } from './components/PostList';
-import { PostForm } from './components/PostForm';
+// import { Form } from './components/Form';
+// import styles from './appStyles.module.css'
+// import { Greet } from './components/Greet';
+// import { Message } from './components/Message';
+// import { ClickHandler } from './components/ClickHandler';
+// import { ParentComponent } from './components/ParentComponent';
+// import { UserGreeting } from './components/UserGreeting';
+// import { NameList } from './components/NameList';
+// import { Stylesheet } from './components/Stylesheet';
+// import { Inline } from './components/Inline';
+// import { PostList } from './components/PostList';
+// import { PostForm } from './components/PostForm';
 
 
 
-function App() {
+function App() { 
+	const [query, setQuery] = useState('')
+	const [inputValue, setInputValue] = useState('')
+	const [isPending, startTransition] = useTransition()
+
+
+	const changeHandler = (event) => {
+		setInputValue(event.target.value)
+		startTransition(() => setQuery(event.target.value))
+	}
+
+
+	const filteredNames = NAMES.filter(item => {
+		return item.first_name.includes(query) || item.last_name.includes(query)
+	})
+
+	
+
 	return (
 		<div className="App">
+			<input type='text' value={inputValue} onChange={changeHandler} />
+			{isPending && <p>Updating List...</p>}
+			{
+				filteredNames.map((item) => (
+					<p key={item.id}>
+						{item.first_name} {item.last_name}
+					</p>
+				))
+			}
+
+
+
+
 			{/* <Greet name="Bruce" heroName="Batman" />
 			<Greet name="Clark" heroName="Superman">
 				<button>Action</button>
@@ -35,10 +68,9 @@ function App() {
 			<Inline/>
 			<h1 className='error'>Error</h1>
 			<h1 className={styles.success}>Success</h1> 
-			<Form />*/}
+			<Form />
 			<PostForm />
-			<PostList />
-
+			<PostList /> */}
 
 		</div>
 	);
