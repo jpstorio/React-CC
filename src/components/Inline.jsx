065ab85bd8cf0f361910fcfaa5fd7d763d2be505
Torio <jpstorio@mymail.mapua.edu.ph>
2022-06-06 11:11:46 +0800

    modify inline.jsx

--- src/components/Inline.jsx
+++ src/components/Inline.jsx
@@ -1,8 +1,8 @@
 export const Inline = () => {
-    const heading = {
-        fontSize: '72px',
-        color: 'blue',
-    }
+    // const heading = {
+    //     fontSize: '72px',
+    //     color: 'blue',
+    // }
     return <h1 className='error'> Inline </h1>
 
 }
